35ac9f53a3688b283ca9567c47102d2dfbe00297
che6single <che@eclipse.org>
2017-10-29 01:04:55 +0000

    commit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first_change1509147537117
+//first_change1509233999795
 /*
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
